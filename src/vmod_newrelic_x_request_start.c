5010b1a8c7fdc0dbf5172f6ad583ed3940203865
Vincent ROBERT <v.robert@of2m.fr>
2015-11-05 07:38:03 +0100

    delete of unwanted function

--- src/vmod_newrelic_x_request_start.c
+++ src/vmod_newrelic_x_request_start.c
@@ -13,26 +13,6 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 	return (0);
 }
 
-VCL_STRING
-vmod_hello(const struct vrt_ctx *ctx, VCL_STRING name)
-{
-	char *p;
-	unsigned u, v;
-
-	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
-	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
-	v++;
-	if (v > u) {
-		/* No space, reset and leave */
-		WS_Release(ctx->ws, 0);
-		return (NULL);
-	}
-	/* Update work space with what we've used */
-	WS_Release(ctx->ws, v);
-	return (p);
-}
-
 VCL_VOID
 vmod_set_x_request_start(const struct vrt_ctx *ctx)
 {
